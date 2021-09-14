2f5926fda3a44dc7896dd65ee978e0143252a37f
Dogl4 <doougllas@hotmail.com.br>
2021-09-14 19:54:37 -0300

    2° Requisito | Cria um form no componente <Login />, que se logado redireciona para o component <Search />.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 
+import { Redirect } from 'react-router';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
+
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      loading: false,
+      loaded: false,
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleClick = () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    createUser({ name }).then((dados) => {
+      if (dados === 'OK') {
+        this.setState({ loading: false, loaded: true });
+      }
+    });
+  };
+
   render() {
+    const MIN_CARACTER = 3;
+    const { name, loading, loaded } = this.state;
+    if (loading) return <Loading />;
+    if (loaded) return <Redirect to="/search" />;
     return (
-      <div data-testid="page-login">
+      <form data-testid="page-login">
         Login
-      </div>
+        <label htmlFor="name-input">
+          Name:
+          <input
+            id="name-input"
+            type="text"
+            name="name"
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <input
+          type="submit"
+          data-testid="login-submit-button"
+          value="Entrar"
+          disabled={ MIN_CARACTER > name.length }
+          onClick={ this.handleClick }
+        />
+      </form>
     );
   }
 }
